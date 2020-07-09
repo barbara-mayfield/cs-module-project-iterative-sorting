d8824173d99d6dacdc0e466ca703fe7cd1f2dc17
Barbara Mayfield <bmayfield1991@gmail.com>
2020-07-09 01:45:06 -0500

    work: linear and binary search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,8 @@
 def linear_search(arr, target):
     # Your code here
-
-
+    for num in range(len(arr)):
+        if arr[num] == target:
+            return num
     return -1   # not found
 
 
@@ -9,6 +10,21 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    start = 0
+    end = (len(arr) - 1)
+    found = False
 
+    while start <= end and not found:
+        # get the middle index
+        mid = (start + end) // 2
 
+        # compare the value in the middle with target
+        # if the middle is the same as target, set found to true
+        if arr[mid] == target:
+            return mid
+        # move start or end index closer to one another, and shrink our search space
+        elif target < arr[mid]:
+            end = mid - 1
+        else:
+            start = mid + 1
     return -1  # not found
